8bc3bdf70db43fd38a0aaa73068ea70765b76ae4
tripzoredd <132893607+tripzoredd@users.noreply.github.com>
2023-05-08 17:58:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.fairy-tail__slider, .our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt="Previous"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="Next"></button>',
+    autoplay: true,
+    fade: true,
+    responsive: [
+    {
+      breakpoint: 601, 
+      settings: {
+        arrows: false
+      }
+    },
+  ]
+  });
+});
